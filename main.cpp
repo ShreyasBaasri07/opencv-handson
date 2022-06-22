a5391e8e7dad42c9efea710be741a236338072c3
Shreyas Baasri <57621491+ShreyasBaasri07@users.noreply.github.com>
2022-06-22 13:07:48 +0530

    reading images and video

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+// images video and webcam
+
+int main() 
+{
+
+
+	// reading an image
+	// string path = "/home/shreyas/opencv-cpp/read_/1.jpeg";
+	// Mat img = imread(path);
+	// imshow("Image", img);
+	// waitKey(0);
+	// return 0;
+
+	// reading video
+	// string path = "/home/shreyas/opencv-cpp/read_/demo.mp4";
+	// VideoCapture cap(path);
+	// Mat img;
+	// while (true) {
+	// 	cap.read(img);
+	// 	imshow("Image", img);
+	// 	waitKey(20);
+	// }
+
+	// accessing web cam
+	// VideoCapture cap(0);
+	// Mat img;
+	// while (true) 
+	// {
+	// 	cap.read(img);
+	// 	imshow("Image", img);
+	// 	waitKey(1);
+	// }
+}
+
+
+
+
+
