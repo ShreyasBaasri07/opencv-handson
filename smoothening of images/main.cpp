36c08bf1a9c7bc2581ccdf7074a88307e03df564
ShreyasBaasri07 <shreyasbaasri22@gmail.com>
2022-06-24 11:07:29 +0530

    update to image smoothening

---
+++ smoothening of images/main.cpp
@@ -0,0 +1,55 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+    // Gaussian blur
+    // Mat image  = imread("/home/shreyas/opencv-cpp/blurring of images/sample.jpeg");
+    // if(image.empty())
+    // {
+    //     cout<<" could not open the image"<<endl;
+    //     return -1;
+    // }
+    // Mat blur_image,blur_image_;
+    // GaussianBlur(image,blur_image,Size(3,3),0);
+    // GaussianBlur(image,blur_image_,Size(5,5),0);
+    // imshow("original image",image);
+    // imshow("blurred image with 3X3 kernel",blur_image);
+    // imshow("blurred image with 5X5 kernel",blur_image_);
+    // imwrite("/home/shreyas/opencv-cpp/blurring of images/gaussian_blur.jpeg",blur_image);
+    // waitKey(0);
+
+    //normal blur
+    // Mat image  = imread("/home/shreyas/opencv-cpp/blurring of images/sample.jpeg");
+    // if(image.empty())
+    // {
+    //     cout<<" could not open the image"<<endl;
+    //     return -1;
+    // }
+    // Mat blur_image;
+    // blur(image,blur_image,Size(3,3));
+    // imshow("original image",image);
+    // imshow("blurred image",blur_image);
+    // imwrite("/home/shreyas/opencv-cpp/blurring of images/normal_blur.jpeg",blur_image);
+    // waitKey(0);
+
+    //median blur
+    // Mat image  = imread("/home/shreyas/opencv-cpp/blurring of images/sample.jpeg");
+    // if(image.empty())
+    // {
+    //     cout<<" could not open the image"<<endl;
+    //     return -1;
+    // }
+    // Mat blur_image;
+    // medianBlur(image,blur_image,(3,3));
+    // imshow("original image",image);
+    // imshow("median blurred image",blur_image);
+    // imwrite("/home/shreyas/opencv-cpp/blurring of images/median_blur.jpeg",blur_image);
+    // waitKey(0);
+    // return 0;
+}
